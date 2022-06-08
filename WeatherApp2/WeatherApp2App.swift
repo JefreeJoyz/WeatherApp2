d1a89ced104f7b7d824d0f94fb283b3aa427cbd7
JefreeJoyz <jefpad12@gmail.com>
2022-06-08 23:57:52 +0300

    Initial Commit

---
+++ WeatherApp2/WeatherApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp2App.swift
+//  WeatherApp2
+//
+//  Created by Eugene Yakushev on 08.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
